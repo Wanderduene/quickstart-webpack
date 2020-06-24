368bb873447258ae197d4e4d8163fb8d9751d142
Ronny Wichards <wichards@iebl.de>
2020-06-24 14:59:11 +0200

    Limit picture size for base64 bundeling and add bmp files to file-loader

--- webpack.common.js
+++ webpack.common.js
@@ -1,36 +1,35 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
-  resolve:{
-    extensions: ['.js','.jsx']
-  },
-  module: {
-    rules:[
-        {
-            test: /\.(jsx|js)$/,
-            exclude: /(node_modules)/,
-            use:{
-                loader:'babel-loader'
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [{
+                test: /\.(jsx|js)$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg|bmp)$/,
+                loader: 'url-loader?limit=1024'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
-        },
-        {
-          test: /\.css$/,
-          use: [ 'style-loader', 'css-loader' ]
-        },
-        { 
-          test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/, 
-          loader: 'url-loader?limit=100000' 
-        },
-        {
-          test: /\.scss$/,
-          use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-        }
-    ]
-  }
+        ]
+    }
 };
